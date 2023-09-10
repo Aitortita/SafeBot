0a9a5c89c09cafb62224d1e642798ead08602396
Aitor <98376581+Aitortita@users.noreply.github.com>
2023-09-10 17:00:35 +0200

    debugging environment variables

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from dotenv import load_dotenv
 import bot
+import os
 
 def configure():
         load_dotenv()
 
 def main():
     configure()
+    print(os.getenv("TOKEN"))
     bot.run_discord_bot()
 
 main()
